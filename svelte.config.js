b44b4724f396a016805736842ee82641a4982c46
DaniRox <danirox.disenioydesarrollo@gmail.com>
2025-03-31 13:03:01 -0300

    Configuración del adaptador Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
-
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
-
-		adapter: adapter()
+		adapter: adapter(),
+		// Otras configuraciones...
 	}
 };
-
